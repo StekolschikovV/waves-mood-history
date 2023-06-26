def4655cfafa942f8b667eac2906fbcec2b365f0
mk <v.a.stekolschikov@gmail.com>
2023-06-26 15:47:26 +0300

    add og

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,26 @@ import Promo from "./components/promo";
 import TimeMachine from "./components/time-machine";
 import FAQ from "./components/FAQ";
 import TopBurners from "./components/top-burners";
+import {Helmet} from "react-helmet";
 
 
 function App() {
     return (
         <>
+            <Helmet>
+                <meta property="og:title" content="Waves Mood History: Draw, Burn, and Shape History Together"
+                      data-rh="true"/>
+                <meta property="og:title" content="Waves Mood History: Draw, Burn, and Shape History Together"/>
+                <meta property="og:description"
+                      content="Immerse yourself in the captivating world of Waves Mood History. Collaborate in real-time, express your feelings through art, and witness the power of burning tokens as strokes on the canvas. Shape a unique history of emotions, where each stroke becomes a permanent mark on the blockchain. Join us and be part of a revolutionary artistic journey."
+                      data-rh="true"/>
+                <meta property="description"
+                      content="Immerse yourself in the captivating world of Waves Mood History. Collaborate in real-time, express your feelings through art, and witness the power of burning tokens as strokes on the canvas. Shape a unique history of emotions, where each stroke becomes a permanent mark on the blockchain. Join us and be part of a revolutionary artistic journey."/>
+                <meta property="og:image"
+                      content="https://raw.githubusercontent.com/StekolschikovV/waves-mood-hostory/main/public/og-image.png"
+                      data-rh="true"/>
+                <title>Waves Mood History</title>
+            </Helmet>
             <Header/>
             <Promo/>
             <MoodCanvas/>
