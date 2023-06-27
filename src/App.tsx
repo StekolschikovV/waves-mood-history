365252ac013bdf7fb29d0e8bb10b09957589c6f8
mk <v.a.stekolschikov@gmail.com>
2023-06-27 20:13:25 +0300

    added Sentry

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React, {useState} from 'react';
-import './App.scss';
-import Header from "./components/header";
-import Promo from "./components/promo";
+import * as Sentry from "@sentry/react";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import useSWR from "swr";
+import './App.scss';
+import Header from "./components/header";
+import Promo from "./components/promo";
 import {Preloader} from "./components/preloader";
+
+Sentry.init({
+    dsn: "https://9cc1778cd77d43fbbc0d206de3230437@o4505432426479616.ingest.sentry.io/4505432432443392",
+    integrations: [
+        new Sentry.BrowserTracing({
+            // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+            tracePropagationTargets: ["localhost", /^https:\/\/waves-mood-history\.com/],
+        }),
+        new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 const MoodCanvasLazy = React.lazy(() => import("./components/mood-canvas"))
 const TopBurnersLazy = React.lazy(() => import("./components/top-burners"))
