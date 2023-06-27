a7c14a796055d0a79db322497a92cca025ed91a1
mk <v.a.stekolschikov@gmail.com>
2023-06-27 14:16:29 +0300

    fixed og

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import './App.scss';
 import Header from "./components/header";
 import Promo from "./components/promo";
-import {Helmet} from "react-helmet";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import useSWR from "swr";
@@ -39,20 +38,6 @@ function App() {
     return (
         <>
             <Preloader isShow={isShowPreloader}/>
-            <Helmet>
-                <meta property="og:title" content="Waves Mood History: Draw, Burn, and Shape History Together"
-                      data-rh="true"/>
-                <meta property="og:title" content="Waves Mood History: Draw, Burn, and Shape History Together"/>
-                <meta property="og:description"
-                      content="Immerse yourself in the captivating world of Waves Mood History. Collaborate in real-time, express your feelings through art, and witness the power of burning tokens as strokes on the canvas. Shape a unique history of emotions, where each stroke becomes a permanent mark on the blockchain. Join us and be part of a revolutionary artistic journey."
-                      data-rh="true"/>
-                <meta property="description"
-                      content="Immerse yourself in the captivating world of Waves Mood History. Collaborate in real-time, express your feelings through art, and witness the power of burning tokens as strokes on the canvas. Shape a unique history of emotions, where each stroke becomes a permanent mark on the blockchain. Join us and be part of a revolutionary artistic journey."/>
-                <meta property="og:image"
-                      content="https://raw.githubusercontent.com/StekolschikovV/waves-mood-hostory/main/public/og-image-2.png"
-                      data-rh="true"/>
-                <title>Waves Mood History</title>
-            </Helmet>
             <Header onLoaded={onLoadedHandler}/>
             <Promo onLoaded={onLoadedHandler}/>
             <MoodCanvasLazy data={data}/>
