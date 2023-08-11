3ba832d9eed2c4ff8933c23049ca2ca971ef608a
mk <v.a.stekolschikov@gmail.com>
2023-08-12 00:40:49 +0300

    fixed positions

--- src/components/mood-canvas3/index.tsx
+++ src/components/mood-canvas3/index.tsx
@@ -6,24 +6,25 @@ import gsap from "gsap";
 import {Color} from "three";
 
 
-function Points() {
-    const [hovered, setHovered] = useState<number | null>(null);
-    const pointsRef = useRef<any>();
-    const timeRef = useRef(0);
+const selected: Map<number, string> = new Map()
 
+
+const Points = ({isSelectMode, color}: { isSelectMode: boolean, color: string }) => {
+
+    const pointsRef = useRef<any>();
 
     const createPositionsArray = (width: number, height: number, step: number) => {
         const positions: number[] = [];
-        for (let y = 0; y < height; y += step) {
-            for (let x = 0; x < width; x += step) {
-                positions.push(x - (height / 2), y - (width / 2), 0);
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                positions.push(((x - (height / 2)) * step) + step / 2, ((y - (width / 2)) * step) + step / 2, 0);
             }
         }
         return positions;
     };
 
     const positions = useMemo(() => {
-        return new Float32Array(createPositionsArray(100, 100, 1));
+        return new Float32Array(createPositionsArray(100, 100, 1.5));
     }, []);
 
 
@@ -33,12 +34,9 @@ function Points() {
 
     const setColor = (position: number, color: string) => {
         const colorObj = new Color(color);
-        if (colors[position * 3] !== colorObj.r)
-            colors[position * 3] = colorObj.r;
-        if (colors[position * 3 + 1] !== colorObj.g)
-            colors[position * 3 + 1] = colorObj.g;
-        if (colors[position * 3 + 2] !== colorObj.b)
-            colors[position * 3 + 2] = colorObj.b;
+        colors[position * 3] = colorObj.r;
+        colors[position * 3 + 1] = colorObj.g;
+        colors[position * 3 + 2] = colorObj.b;
     }
 
     const setAnimation = (position: number) => {
@@ -47,11 +45,12 @@ function Points() {
             const tl = gsap.timeline()
             tl
                 .to([positionAttribute.array], {
-                    duration: 2,
-                    [position * 3 + 2]: 10,
+                    duration: 0.3,
+                    [position * 3 + 2]: 3,
                 })
                 .to([positionAttribute.array], {
-                    duration: 2,
+
+                    duration: 1,
                     [position * 3 + 2]: 0,
                 })
         }
@@ -61,87 +60,37 @@ function Points() {
         if (pointsRef.current) {
             const positionAttribute = pointsRef.current.geometry.getAttribute('position');
             const colorAttribute = pointsRef.current.geometry.getAttribute('color');
-            const p = hovered
-            // if (p) {
-            for (let i = 0; i < positionAttribute.count; i++) {
-
-                // if (positionAttribute.array[i * 3 + 2] === 10) {
-                //     gsap.to([positionAttribute.array], {
-                //         duration: 2,
-                //         [i * 3 + 2]: 0, // Опустить частицу
-                //     });
-                // }
-                // // console.log(positionAttribute.array[i * 3 + 2])
-                // if (hovered === i) {
-                //     if (positionAttribute.array[i * 3 + 2] === 0) {
-                //         gsap.to([positionAttribute.array], {
-                //             duration: 1,
-                //             [i * 3 + 2]: 10, // Поднять частицу
-                //         });
-                //     }
-                // }
-            }
-            // for (let i = 0; i < positionAttribute.count; i++) {
-            //     //     const x = positionAttribute.getX(i);
-            //     //     const y = positionAttribute.getY(i);
-            //     //     positionAttribute.setXYZ(i, x, y, Math.sin(clock.elapsedTime + x * 0.1 + y * 0.1));
-            //     //
-            //     //     const t = (Math.sin(timeRef.current + x * 0.1 + y * 0.1) + 1) / 2; // Нормализуем от -1 до 1 к 0 до 1
-            //     //     const colorValue = (1 - t) * 0xff + t * 0x00; // Линейная интерполяция между красным и синим
-            //     //     colorAttribute.setXYZ(i, colorValue / 255, 0, (255 - colorValue) / 255); // Преобразуем 0-255 в 0-1
-            //
-            //
-            // }
-            // // const colorAttribute = pointsRef.current.material.getAttribute('color');
-            // for (let i = 0; i < positionAttribute.count; i++) {
-            //     const x = positionAttribute.getX(i);
-            //     const y = positionAttribute.getY(i);
-            //     positionAttribute.setXYZ(i, x, y, 0);
-            // }
-            // // positionAttribute[hovered || 0].setXYZ(10, 10, 10)
-            // const x = positionAttribute.getX(p);
-            // const y = positionAttribute.getY(p);
-            // positionAttribute.setXYZ(p, x, y, 2)
-            // // colorAttribute.set("red")
-            // // colorAttribute.setXYZ(hovered || 0, new Color("red")); // Преобразуем 0-255 в 0-1
-            //
-            if (p) {
-                setColor(p, "red")
-                setAnimation(p)
-            }
-
-
             colorAttribute.needsUpdate = true;
             positionAttribute.needsUpdate = true;
-            // timeRef.current += clock.getDelta();
-            // }
-
         }
     });
+
+    const select = (position: number, type: "select" | "click"): void => {
+        if ((isSelectMode || type === "click") && selected.get(position) !== color) {
+            setColor(position, "red")
+            setAnimation(position)
+            selected.set(position, color)
+        }
+    }
 
     return (
         <points
             ref={pointsRef}
-            onPointerOver={(event) => {
-                setHovered(event.index || 0)
-            }}
-            onPointerOut={() => setHovered(null)}
+            onPointerOver={(event) => select(event.index as number, "select")}
+            onClick={(event) => select(event.index as number, "click")}
         >
             <bufferGeometry attach="geometry">
                 <bufferAttribute
-                    needsUpdate
                     attach="attributes-position"
                     array={positions}
                     count={(positions?.length || 0) / 3} //
                     itemSize={3}
                 />
                 <bufferAttribute
-                    needsUpdate
                     attach="attributes-color"
                     array={colors}
                     count={(positions?.length || 0) / 3} //
-                    itemSize={3}
-                    normalized/>
+                    itemSize={3}/>
 
             </bufferGeometry>
             <pointsMaterial size={1} color={""} vertexColors={true}/>
@@ -153,8 +102,10 @@ function Points() {
 export default observer(function MoodCanvas3() {
 
 
-    const width = window.innerWidth > 700 ? "550px" : "90vw"
-    const height = window.innerWidth > 700 ? "500px" : "80vw"
+    const [isSelectMode, setIsSelectMode] = useState(false)
+
+    const width = window.innerWidth > 700 ? "600px" : "90vw"
+    const height = window.innerWidth > 700 ? "600px" : "80vw"
 
 
     return <>
@@ -193,11 +144,11 @@ export default observer(function MoodCanvas3() {
                         cursor: "crosshair",
                         border: "1px solid black"
                     }}
-                    // onMouseDown={() => window.app.onMouseDown = true}
-                    // onMouseUp={() => window.app.onMouseDown = false}
+                    onMouseDown={() => setIsSelectMode(true)}
+                    onMouseUp={() => setIsSelectMode(false)}
                 >
-                    <CANVAS camera={{fov: 75, position: [0, 0, 70],}}>
-                        <Points/>
+                    <CANVAS camera={{fov: 75, position: [0, 0, 100]}}>
+                        <Points isSelectMode={isSelectMode} color={"red"}/>
                     </CANVAS>
                 </div>
                 {/*            </div>*/}
